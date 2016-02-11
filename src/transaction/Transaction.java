7f85f45d96c460193c92fbf38747360890765d31
Daniel Roberts <Ademan555@gmail.com>
2016-02-10 23:27:15 -0800

    Initial commit of Transaction related classes

---
+++ src/transaction/Transaction.java
@@ -0,0 +1,22 @@
+package transaction;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import post.Customer;
+
+public class Transaction {
+	private Customer customer;
+	private List<LineItem> items;
+	private Payment payment;
+
+	public Transaction(Customer customer, List<LineItem> items, Payment payment) {
+		this.customer = customer;
+		this.items = items;
+		this.payment = payment;	
+	}
+
+	public List<LineItem> getItems() { return items; }
+	public Payment getPayment() { return payment; }
+	public Customer getCustomer() { return customer; }
+}
