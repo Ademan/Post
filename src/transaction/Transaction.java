dea60c187be21e9eb648c285a07c70dc032a1fe9
Daniel Roberts <Ademan555@gmail.com>
2016-02-12 12:53:54 -0800

    Add total price calculation to Transaction

--- src/transaction/Transaction.java
+++ src/transaction/Transaction.java
@@ -7,6 +7,8 @@ import post.Customer;
 
 import payment.Payment;
 
+import storefile.Item;
+
 public class Transaction {
 	private Customer customer;
 	private List<LineItem> items;
@@ -21,4 +23,16 @@ public class Transaction {
 	public List<LineItem> getItems() { return items; }
 	public Payment getPayment() { return payment; }
 	public Customer getCustomer() { return customer; }
+
+	public float getAmountDue() {
+		double amountDue = 0.0;
+
+		for (LineItem lineItem : items) {
+			Item item = lineItem.getItem();
+
+			amountDue += item.getPrice() * lineItem.getQuantity();
+		}
+
+		return (float)amountDue;
+	}
 }
